65536bc0ea45264ca2214c319f0b2aa39258d847
Yusha <yusha.aziz@live.com>
2026-08-09 23:39:09 +0000

    upload data from open food csv to postgres database

--- pipeline.py
+++ pipeline.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
 
 # Stops column names from being truncated when printed to terminal
 pd.options.display.max_columns = None
@@ -6,33 +8,54 @@ pd.options.display.max_rows = None
 
 url = 'https://static.openfoodfacts.org/data/en.openfoodfacts.org.products.csv.gz'
 
+engine = create_engine('postgresql://root:root@localhost:5432/open_food')
+
 # Extraction
 def extract_csv(url, nrows=None):       
-    open_food_df = pd.read_csv(
+    df_iter = pd.read_csv(
         url, 
         sep='\t',
         nrows=nrows,
-        low_memory=False
+        iterator=True,
+        low_memory=False,
+        chunksize=100000
         )
-    return open_food_df
+    
+    for df_chunk in tqdm(df_iter):
+        df_chunk.to_sql(name='open_food', con=engine, if_exists='replace');
+    
+    return df_iter
 
 # Transform original dataframe to only include certain fields: foods from Australia only, etc.
-def transform(Dataframe):
+def transform_dataframe(Dataframe):
     return None
 
 # Load open food df into Postgres DB.
-def load(Dataframe):
-    return None
+def load_df_to_database(Dataframe):
+    for df_chunk in tqdm(Dataframe):
+        df_chunk.to_sql(name='open_food', con=engine, if_exists='replace');
 
-if __name__ == "__main__":
+def main():
+    
+    # open_food_df_iter = 
 
-    open_food_df = extract_csv(url, 100000)
+    extract_csv(url, 300000)
 
-    open_food_df = transform(open_food_df)
+    # open_food_dataframe = pd.concat(open_food_df_iter, ignore_index=True)
 
-    # open_food_columns_list = open_food_df.columns.tolist()
+    # open_food_df = transform_dataframe(open_food_df)
+
+    # load_df_to_database(open_food_df_iter)
+
+    # open_food_columns_list = open_food_dataframe.columns.tolist()
 
     # for column in open_food_columns_list:
-    #     print(column)
+        # print(column)
 
-    open_food_df.to_csv('output.csv', index=False)
+    print("Finished")
+
+    # open_food_df.to_csv('output.csv', index=False)
+
+if __name__ == "__main__":
+    main()
+
